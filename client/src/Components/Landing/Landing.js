f4c0f1299148812f7ffb46af87f2c37d6095ada8
uhNexus <gottschalkmitch@gmail.com>
2021-11-18 16:10:31 -0500

    troubleshooting image sources

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -8,6 +8,7 @@ const Landing = () => {
     <>
       <div className="landing">
         <h3 className="landing__txt">Trendy jewelry at an affordable price.</h3>
+        <img className="landing__img1" src={cuff} alt="Product image" />
       </div>
     </>
   );
